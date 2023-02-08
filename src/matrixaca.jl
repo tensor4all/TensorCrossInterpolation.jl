186b489ec2e606cef60a85b048742eebe898c1e1
Marc Ritter <Ritter.Marc@physik.uni-muenchen.de>
2023-02-08 13:01:21 +0100

    updated matrixaca to near-complete functionality

--- src/matrixaca.jl
+++ src/matrixaca.jl
@@ -4,12 +4,22 @@ mutable struct MatrixACA{T} <: AbstractMatrixCI{T}
 
     u::Matrix{T} # u_k(x): (nrows, npivot)
     v::Matrix{T} # v_k(y): (npivot, ncols)
-    alpha::Vector{T}
+    alpha::Vector{T} # 1/δ: npivot
 
     function MatrixACA{T}(
         nrows::Int, ncols::Int
     ) where {T<:Number}
         return new{T}(Int[], Int[], zeros(nrows, 0), zeros(0, ncols), T[])
+    end
+
+    function MatrixACA{T}(
+        A::AbstractMatrix{T},
+        firstpivot::Union{CartesianIndex{2},Tuple{Int,Int},Pair{Int,Int}}
+    ) where {T<:Number}
+        return new{T}(
+            [firstpivot[1]], [firstpivot[2]],
+            A[:, [firstpivot[2]]], A[[firstpivot[1]], :],
+            [1 / A[firstpivot[1], firstpivot[2]]])
     end
 end
 
@@ -17,11 +27,12 @@ ncols(aca::MatrixACA) = size(aca.u, 1)
 nrows(aca::MatrixACA) = size(aca.v, 2)
 npivots(aca::MatrixACA) = size(aca.u, 2)
 
-function adaptivecrossinterpolate(
-    ::Type{T},
-    tolerance=1e-6,
-    maxiter=200,
-    firstpivot=CartesianIndex(1,1)) where T
+function availablerows(aca::MatrixACA{T}) where {T}
+    return setdiff(1:nrows(aca), aca.rowindices)
+end
+
+function availablecols(aca::MatrixACA{T}) where {T}
+    return setdiff(1:ncols(aca), aca.colindices)
 end
 
 """
@@ -34,7 +45,7 @@ function uk(aca::MatrixACA{T}, A) where {T}
     u, v = aca.u, aca.v
     for l in 1:k-1
         xl = aca.rowindices[l]
-        @. result -= (v[l,yk]/u[xl,l]) * u[:, l]
+        @. result -= (v[l, yk] / u[xl, l]) * u[:, l]
     end
     return result
 end
@@ -49,30 +60,50 @@ function vk(aca::MatrixACA{T}, A) where {T}
     u, v = aca.u, aca.v
     for l in 1:k-1
         xl = aca.rowindices[l]
-        @. result -= (u[xk,l]/u[xl,l]) * v[l, :]
+        @. result -= (u[xk, l] / u[xl, l]) * v[l, :]
     end
     return result
 end
 
+"""
+    function addpivot!(a::AbstractMatrix{T}, aca::MatrixACA{T})
 
+Find and add a new pivot according to the ACA algorithm in Kumar 2016 ()
+"""
 function addpivot!(
     a::AbstractMatrix{T},
     aca::MatrixACA{T},
-    pivotindices::Union{CartesianIndex{2},Tuple{Int,Int},Pair{Int,Int}}) where T
-    i, j = pivotindices
-    push!(aca.rowindices, i)
-    push!(aca.colindices, j)
+    pivotindices::Union{CartesianIndex{2},Tuple{Int,Int},Pair{Int,Int},Nothing}=nothing
+) where {T}
+    xk::Int = 0
+    yk::Int = 0
+
+    # Add col
+    if isnothing(pivotindices)
+        availcols = availablecols(aca)
+        yk = availcols[argmax(abs.(aca.v[end, availcols]))]
+    else
+        yk = pivotindices[2]
+    end
+    push!(aca.colindices, yk)
     aca.u = hcat(aca.u, uk(aca, a))
+
+    # Add row
+    if isnothing(pivotindices)
+        availrows = availablerows(aca)
+        xk = availrows[argmax(abs.(aca.u[availrows, end]))]
+    else
+        xk = pivotindices[1]
+    end
+    push!(aca.rowindices, xk)
     aca.v = vcat(aca.v, transpose(vk(aca, a)))
-    aca.alpha = vcat(aca.alpha, 1/aca.u[i, end])
-    return nothing
+
+    push!(aca.alpha, 1 / aca.u[xk, end])
 end
 
-
-function evaluate(aca::MatrixACA{T})::Matrix{T} where T
+function evaluate(aca::MatrixACA{T})::Matrix{T} where {T}
     return aca.u * Diagonal(aca.alpha) * aca.v
 end
-
 
 function evaluate(aca::MatrixACA{T}, i::Int, j::Int)::T where {T}
     if isempty(aca)
@@ -81,6 +112,15 @@ function evaluate(aca::MatrixACA{T}, i::Int, j::Int)::T where {T}
         np = npivots(aca)
         left = reshape(view(aca.u, i, :), 1, np)
         right = reshape(view(aca.v, :, j), np, 1)
-        return (left * Diagonal(aca.alpha) * right)[1,1]
+        return (left*Diagonal(aca.alpha)*right)[1, 1]
     end
+end
+
+function adaptivecrossinterpolate(
+    a::AbstractMatrix{T};
+    tolerance=1e-6,
+    maxiter=200,
+    firstpivot=argmax(abs.(a))
+) where {T}
+    aca = MatrixACA(a, firstpivot)
 end
--- test/test_matrixaca.jl
+++ test/test_matrixaca.jl
@@ -8,14 +8,37 @@ import TensorCrossInterpolation as TCI
 @testset "MatrixACA" begin
     @testset "2x2 matrix" begin
         A = [
-            1.0 0.1
-           -0.1 2.0
+            1.0 0.1 -1.0
+           -0.1 2.0 -1.0
+            0.5 0.2 0.3
         ]
-        aca = TCI.MatrixACA{Float64}(2, 2)
-        TCI.addpivot!(A, aca, (1,1))
-        TCI.addpivot!(A, aca, (2,2))
+
+        # aca = TCI.MatrixACA{Float64}(2, 2)
+        # TCI.addpivot!(A, aca, (1,1))
+
+        aca = TCI.MatrixACA{Float64}(A, (1, 1))
+
+        @test TCI.ncols(aca) == 3
+        @test TCI.nrows(aca) == 3
+
+        @test TCI.npivots(aca) == 1
+        @test aca.rowindices == [1]
+        @test aca.colindices == [1]
+
+        TCI.addpivot!(A, aca, (2, 3))
+
+        @test TCI.npivots(aca) == 2
+        @test aca.rowindices == [1, 2]
+        @test aca.colindices == [1, 3]
+
+        TCI.addpivot!(A, aca)
+
+        @test TCI.npivots(aca) == 3
+        @test aca.rowindices == [1, 2, 3]
+        @test aca.colindices == [1, 3, 2]
+
         @test TCI.evaluate(aca) ≈ A
-        @test [TCI.evaluate(aca, i, j) for i in 1:2, j in 1:2] ≈ A
+        @test [TCI.evaluate(aca, i, j) for i in 1:3, j in 1:3] ≈ A
     end
 end
 
