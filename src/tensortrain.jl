98490cbddeaa0d3ce44bd03c1e91bb8f02d00842
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2023-07-12 21:25:56 +0900

    Add TTCache

--- src/tensortrain.jl
+++ src/tensortrain.jl
@@ -14,11 +14,11 @@ The corresponding function is:
 
 Evaluates the tensor train `tt` at indices given by `indexset`.
 """
-mutable struct TensorTrain{ValueType, N} <: CachedTensorTrain{ValueType}
-    T::Vector{Array{ValueType, N}}
-    cache::Vector{Dict{MultiIndex, Matrix{ValueType}}}
+mutable struct TensorTrain{ValueType,N} <: CachedTensorTrain{ValueType}
+    T::Vector{Array{ValueType,N}}
+    cache::Vector{Dict{MultiIndex,Matrix{ValueType}}}
 
-    function TensorTrain{ValueType, N}(T::Vector{Array{ValueType, N}}) where {ValueType, N}
+    function TensorTrain{ValueType,N}(T::Vector{Array{ValueType,N}}) where {ValueType,N}
         for i in 1:length(T)-1
             if (last(size(T[i])) != size(T[i+1], 1))
                 throw(ArgumentError(
@@ -27,7 +27,7 @@ mutable struct TensorTrain{ValueType, N} <: CachedTensorTrain{ValueType}
             end
         end
 
-        new{ValueType, N}(T, [Dict{MultiIndex, Matrix{ValueType}}() for _ in 1:length(T)])
+        new{ValueType,N}(T, [Dict{MultiIndex,Matrix{ValueType}}() for _ in 1:length(T)])
     end
 end
 
@@ -38,8 +38,8 @@ Create a tensor train out of a vector of tensors. Each tensor should have links 
 previous and next tensor as dimension 1 and 3, respectively; the local index ("physical
 index" for MPS in physics) is dimension 2.
 """
-function TensorTrain(T::Vector{Array{V, N}}) where {V, N}
-    return TensorTrain{V, N}(T)
+function TensorTrain(T::Vector{Array{V,N}}) where {V,N}
+    return TensorTrain{V,N}(T)
 end
 
 """
@@ -49,8 +49,8 @@ Convert the TCI object into a tensor train, also known as an MPS.
 
 See also: [`crossinterpolate`](@ref), [`TensorCI`](@ref)
 """
-function TensorTrain(tci::TensorCI{V})::TensorTrain{V, 3} where {V}
-    return TensorTrain{V, 3}(TtimesPinv.(tci, 1:length(tci)))
+function TensorTrain(tci::TensorCI{V})::TensorTrain{V,3} where {V}
+    return TensorTrain{V,3}(TtimesPinv.(tci, 1:length(tci)))
 end
 
 """
@@ -60,7 +60,7 @@ Convert the TCI2 object into a tensor train, also known as an MPS.
 
 See also: [`crossinterpolate`](@ref), [`TensorCI`](@ref)
 """
-function TensorTrain(tci::TensorCI2{V})::TensorTrain{V, 3} where {V}
+function TensorTrain(tci::TensorCI2{V})::TensorTrain{V,3} where {V}
     return TensorTrain(tci.T)
 end
 
@@ -68,13 +68,161 @@ function tensortrain(tci)
     return TensorTrain(tci)
 end
 
-function ttcache(tt::TensorTrain{V, N}, b::Int) where {V, N}
+function ttcache(tt::TensorTrain{V,N}, b::Int) where {V,N}
     return tt.cache[b]
 end
 
-function emptycache!(tt::TensorTrain{V, N}) where {V, N}
+function emptycache!(tt::TensorTrain{V,N}) where {V,N}
     for d in tt.cache
         empty!(d)
     end
     nothing
+end
+
+
+
+"""
+    struct TTCache{ValueType, N}
+
+Represents a Matrix Product Operator / Tensor Train with N legs on each tensor.
+"""
+struct TTCache{ValueType} <: CachedTensorTrain{ValueType}
+    T::Vector{Array{ValueType}}
+    cacheleft::Vector{Dict{MultiIndex,Vector{ValueType}}}
+    cacheright::Vector{Dict{MultiIndex,Vector{ValueType}}}
+
+    function TTCache(T::AbstractVector{<:AbstractArray{ValueType,3}}) where {ValueType}
+        new{ValueType}(
+            T,
+            [Dict{MultiIndex,Vector{ValueType}}() for _ in T],
+            [Dict{MultiIndex,Vector{ValueType}}() for _ in T])
+    end
+end
+
+function TTCache(TT::AbstractTensorTrain{ValueType}) where {ValueType}
+    TTCache(TT.T)
+end
+
+function ttcache(tt::TTCache{V}, leftright::Symbol, b::Int) where {V}
+    if leftright == :left
+        return tt.cacheleft[b]
+    elseif leftright == :right
+        return tt.cacheright[b]
+    else
+        throw(ArgumentError("Choose from left or right cache."))
+    end
+end
+
+function evaluateleft(
+    tt::TTCache{V},
+    indexset::AbstractVector{Int}
+)::Vector{V} where {V}
+    if length(indexset) > length(tt)
+        throw(ArgumentError("For evaluateleft, number of indices must be smaller or equal to the number of TTCache legs."))
+    end
+
+    ell = length(indexset)
+    if ell == 0
+        return V[1]
+    elseif ell == 1
+        return vec(tt[1][:, indexset[1], :])
+    end
+
+    cache = ttcache(tt, :left, ell)
+    key = collect(indexset)
+    if !(key in keys(cache))
+        cache[key] = vec(reshape(evaluateleft(tt, indexset[1:ell-1]), 1, :) * tt[ell][:, indexset[ell], :])
+    end
+    return cache[key]
+end
+
+function evaluateright(
+    tt::TTCache{V},
+    indexset::AbstractVector{Int}
+)::Vector{V} where {V}
+    if length(indexset) > length(tt)
+        throw(ArgumentError("For evaluateright, number of indices must be smaller or equal to the number of TTCache legs."))
+    end
+
+    if length(indexset) == 0
+        return V[1]
+    elseif length(indexset) == 1
+        return vec(tt[end][:, indexset[1], :])
+    end
+    ell = length(tt) - length(indexset) + 1
+
+    cache = ttcache(tt, :right, ell)
+    key = collect(indexset)
+    if !(key in keys(cache))
+        cache[key] = tt[ell][:, indexset[1], :] * evaluateright(tt, indexset[2:end])
+    end
+    return cache[key]
+end
+
+function evaluate(
+    tt::TTCache{V},
+    indexset::AbstractVector{Int};
+    usecache::Bool=true
+)::V where {V}
+    if length(tt) != length(indexset)
+        throw(ArgumentError("To evaluate a tensor train of length $(length(tt)), need $(length(tt)) index values, but only got $(length(indexset))."))
+    end
+    if usecache
+        midpoint = div(length(tt), 2)
+        return sum(
+            evaluateleft(tt, indexset[1:midpoint]) .*
+            evaluateright(tt, indexset[midpoint+1:end]))
+    else
+        return sum(prod(T[:, i, :] for (T, i) in zip(tt, indexset)))
+    end
+end
+
+
+function batchevaluate(
+    tt::TTCache{V},
+    leftindexset::AbstractVector{MultiIndex},
+    rightindexset::AbstractVector{MultiIndex},
+    ::Val{M}
+)::Array{V,M + 2} where {V,M}
+    N = length(tt.T)
+    nleft = length(leftindexset[1])
+    nright = length(rightindexset[1])
+    nleftindexset = length(leftindexset)
+    nrightindexset = length(rightindexset)
+    ncent = N - nleft - nright
+
+    if ncent != M
+        error("Invalid parameter M: $(M)")
+    end
+
+    DL = nleft == 0 ? 1 : size(tt.T[nleft], 3)
+    lenv = ones(V, nleftindexset, DL)
+    if nleft > 0
+        for (il, lindex) in enumerate(leftindexset)
+            lenv[il, :] .= evaluateleft(tt, lindex)
+        end
+    end
+
+    DR = nright == 0 ? 1 : size(tt.T[nleft+ncent+1], 1)
+    renv = ones(V, DR, nrightindexset)
+    if nright > 0
+        for (ir, rindex) in enumerate(rightindexset)
+            renv[:, ir] .= evaluateright(tt, rindex)
+        end
+    end
+
+    localdim = zeros(Int, ncent)
+    for n in nleft+1:(N-nright)
+        # (nleftindexset, d, ..., d, D) x (D, d, D)
+        T_ = tt.T[n]
+        localdim[n-nleft] = size(T_, 2)
+        bonddim_ = size(T_, 1)
+        lenv = reshape(lenv, :, bonddim_) * reshape(T_, bonddim_, :)
+    end
+
+    # (nleftindexset, d...d * D) x (D, nrightindexset)
+    bonddim_ = size(renv, 1)
+    lenv = reshape(lenv, :, bonddim_) * reshape(renv, bonddim_, :)
+
+    return reshape(lenv, nleftindexset, localdim..., nrightindexset)
 end
