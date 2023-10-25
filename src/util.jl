f58c62da6db96a5397e6c832c7d395faebaf31a1
Marc Ritter <Ritter.Marc@physik.uni-muenchen.de>
2023-10-25 16:58:27 +0200

    fix: randomsubset for empty set

--- src/util.jl
+++ src/util.jl
@@ -33,10 +33,16 @@ function isconstant(collection)
     return all(collection .== c)
 end
 
-function randomsubset(set, n::Int)
+function randomsubset(
+    set::AbstractArray{ValueType}, n::Int
+)::Array{ValueType} where {ValueType}
     n = min(n, length(set))
+    if n <= 0
+        return ValueType[]
+    end
+
     c = copy(set[:])
-    subset = Array{typeof(first(c))}(undef, n)
+    subset = Array{ValueType}(undef, n)
     for i in 1:n
         index = rand(1:length(c))
         subset[i] = c[index]
