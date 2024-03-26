f8ddc26271a041e16b5e0b734b0d453a81370d3a
Marc Ritter <Ritter.Marc@physik.uni-muenchen.de>
2024-03-26 12:53:13 -0400

    added documentation for integrate()

--- src/integration.jl
+++ src/integration.jl
@@ -1,17 +1,42 @@
+"""
+    function integrate(
+        ::Type{ValueType},
+        f,
+        a::Vector{ValueType},
+        b::Vector{ValueType};
+        tolerance=1e-8,
+        GKorder::Int=15
+    ) where {ValueType}
+
+Integrate the function `f` using TCI and Gauss--Kronrod quadrature rules.
+
+Arguments:
+- ValueType: return type of `f`.
+- a: Vector of lower bounds in each dimension. Effectively, the lower corner of the hypercube that is being integrated over.
+- b: Vector of upper bounds in each dimension.
+- tolerance: tolerance of the TCI approximation for the values of f.
+- GKorder: Order of the Gauss--Kronrod rule, e.g. 15.
+"""
 function integrate(
     ::Type{ValueType},
     f,
     a::Vector{ValueType},
     b::Vector{ValueType};
     tolerance=1e-8,
-    order::Int=15
+    GKorder::Int=15
 ) where {ValueType}
-    @assert isodd(order)
-    @assert length(a) == length(b)
-    nodes1d, weights1d, _ = QuadGK.kronrod(order ÷ 2, -1, +1)
+    if iseven(order)
+        error("Gauss--Kronrod order must be odd, e.g. 15 or 61.")
+    end
+
+    if length(a) != length(b)
+        error("Integral bounds must have the same dimensionality, but got $(length(a)) lower bounds and $(length(b)) upper bounds.")
+    end
+
+    nodes1d, weights1d, _ = QuadGK.kronrod(GKorder ÷ 2, -1, +1)
     nodes = @. (b - a) * (nodes1d' + 1) / 2 + a
     weights = @. (b - a) * weights1d' / 2
-    normalization = order^length(a)
+    normalization = GKorder^length(a)
 
     localdims = fill(length(nodes1d), length(a))
 
