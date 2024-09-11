5f8ab768d99cc5c232f462b05353f947141246e7
Simone.Fodera <simone.fodera@th-ws-7010m46.theorie.physik.uni-muenchen.de>
2024-09-11 13:18:54 +0200

    Add average and weightedsum functions

--- src/abstracttensortrain.jl
+++ src/abstracttensortrain.jl
@@ -175,6 +175,38 @@ function sum(tt::AbstractTensorTrain{V}) where {V}
     return only(v)
 end
 
+"""
+    function average(tt::TensorTrain{V}) where {V}
+
+Evaluates the average of the tensor train approximation over all lattice sites in an efficient
+factorized manner.
+"""
+function average(tt::AbstractTensorTrain{V}) where {V}
+    v = transpose(sum(tt[1], dims=(1, 2))[1, 1, :]) / length(tt[1][1, :, 1])
+    for T in tt[2:end]
+        v *= sum(T, dims=2)[:, 1, :] / length(T[1, :, 1])
+    end
+    return only(v)
+end
+
+"""
+    function weightedsum(tt::TensorTrain{V}, w::Vector{V}) where {V}
+
+Evaluates the weighted sum of the tensor train approximation over all lattice sites in an efficient
+factorized manner, where w is the vector of vector of weights which has the same length and the same sizes as tt.
+"""
+function weightedsum(tt::AbstractTensorTrain{V}, w::Vector{Vector{V}}) where {V}
+    length(tt) == length(w) || throw(DimensionMismatch("The length of the Tensor Train is different from the one of the weight vector ($(length(tt)) and $(length(w)))."))
+    size(tt[1])[2] == length(w[1]) || throw(DimensionMismatch("The dimension at site 1 of the Tensor Train is different from the one of the weight vector ($(size(tt[1])[2]) and $(length(w[1])))."))
+    v = transpose(sum(tt[1].*w[1]', dims=(1, 2))[1, 1, :])
+    for i in 2:length(tt)
+        size(tt[i])[2] == length(w[i]) || throw(DimensionMismatch("The dimension at site $(i) of the Tensor Train is different from the one of the weight vector ($(size(tt[i])[2]) and $(length(w[i])))."))
+        v *= sum(tt[i].*w[i]', dims=2)[:, 1, :]
+    end
+    return only(v)
+end
+
+
 function _addtttensor(
     A::Array{V}, B::Array{V};
     factorA=one(V), factorB=one(V),
