3d214aff08b2a2826f16a58b391ab94cdf79e58d
Marc Ritter <Ritter.Marc@physik.uni-muenchen.de>
2024-09-18 13:33:36 +0200

    updated JET test compatibility cutoff to julia 1.10

--- test/test_with_jet.jl
+++ test/test_with_jet.jl
@@ -2,7 +2,7 @@ using JET
 import TensorCrossInterpolation as TCI
 
 @testset "JET" begin
-    if VERSION ≥ v"1.9"
+    if VERSION ≥ v"1.10"
         JET.test_package(TCI; target_defined_modules=true)
     end
 end
